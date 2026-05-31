78af4f02f045e9113821c4a24b616cec03411c85
MansfieldPlumbing <109449446+MansfieldPlumbing@users.noreply.github.com>
2026-05-31 15:02:53 -0400

    Fix embedded repo folder

---
+++ repo/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'q-gallery-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
